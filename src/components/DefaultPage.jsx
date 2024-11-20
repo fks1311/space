1717117c6d37d046c83786e269d4ce92de9b0bf6
ranyoung <fks1311@gmail.com>
2024-11-20 20:36:58 +0900

    data.json 파일 추가 및 기본 페이지 레이아웃 수정

--- src/components/DefaultPage.jsx
+++ src/components/DefaultPage.jsx
@@ -2,7 +2,7 @@ import { forwardRef } from "react";
 import styled from "styled-components";
 
 export const DefaultPage = forwardRef((props, ref) => {
-  return <Layout>{props.children}</Layout>;
+  return <Layout className="defaultpage">{props.children}</Layout>;
 });
 const Layout = styled.div`
   height: 100%;
